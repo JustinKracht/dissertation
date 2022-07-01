e1b6c57003d4bcf1a9874c1ea31d82c4c784bca3
Justin Kracht <justin.kracht@pm.me>
2022-07-01 13:06:25 -0500

    WIP

--- misc/misc_code.R
+++ misc/misc_code.R
@@ -76,7 +76,96 @@ ggsave(here("img/rmsea_conditional_cfi_with_factor_cor.png"),
        height = 10,
        width = 8,
        dpi = 320)
+
 saveRDS(rmsea_cfi_vals, file = here("data/rmsea_cfi_vals_with_factor_cor.RDS"))
+
+
+# CFI Conditional RMSEA with Factor Corr ----------------------------------
+
+reps <- 50
+
+factors <- unique(results_matrix$factors)
+items_per_factor <- unique(results_matrix$items_per_factor)
+loading <- unique(results_matrix$loading_numeric)
+factor_cor <- c(0, 0.6)
+target_cfi <- seq(0.90, 0.99, by = 0.005)
+
+conditions_matrix <- expand.grid(
+  factors = factors,
+  items_per_factor = items_per_factor,
+  loading = loading,
+  factor_cor = factor_cor,
+  target_cfi = target_cfi
+)
+
+set.seed(666)
+cfi_rmsea_vals <- pblapply(
+  X = 1:nrow(conditions_matrix), 
+  FUN = function(condition) {
+    mod <- simFA(
+      Model = list(NFac = conditions_matrix$factors[condition],
+                   NItemPerFac = conditions_matrix$items_per_factor[condition],
+                   Model = "oblique"),
+      Phi = list(MaxAbsPhi = conditions_matrix$factor_cor[condition],
+                 PhiType = "fixed"),
+      Loadings = list(FacLoadDist = "fixed",
+                      FacLoadRange = conditions_matrix$loading[condition])
+    )
+    
+    map_dfr(.x = 1:reps,
+            .f = function(x, target_cfi) {
+              sol <- noisemaker(mod = mod, 
+                                method = "TKL", 
+                                target_rmsea = NULL,
+                                target_cfi = target_cfi,
+                                tkl_ctrl = list(penalty = 1e6,
+                                                NWmaxLoading = 2,
+                                                WmaxLoading = .3))
+              w_constraints_violated <- sum(sol$W[,1] >= .3) > 2
+              c(rmsea = sol$rmsea, cfi = sol$cfi, 
+                w_constraints_violated = w_constraints_violated)
+            }, target_cfi = conditions_matrix$target_cfi[condition])
+  }
+)
+
+cfi_rmsea_vals <- bind_rows(cfi_rmsea_vals)
+cfi_rmsea_vals <- bind_cols(
+  cfi_rmsea_vals,
+  conditions_matrix[rep(1:nrow(conditions_matrix), each = reps),]
+)
+
+cfi_rmsea_vals <- cfi_rmsea_vals %>%
+  mutate(
+    factors_rec = factor(factors, 
+                         labels = levels(results_matrix$factors_rec)),
+    loading_rec = factor(loading, 
+                         labels = levels(results_matrix$loading_rec)),
+    factor_cor_rec = factor(factor_cor,
+                            labels = levels(results_matrix$factor_cor_rec)[c(1,3)]),
+    items_per_factor_rec = factor(items_per_factor, 
+                                  labels = levels(results_matrix$items_per_factor_rec))
+  )
+
+saveRDS(cfi_rmsea_vals, file = here("data/cfi_conditional_rmsea_with_factor_cor.RDS"))
+
+cfi_rmsea_vals %>%
+  filter(rmsea < 1) %>% 
+  ggplot(aes(y = rmsea, x = cfi)) +
+  geom_point(size = 1, alpha = .1) +
+  geom_abline(aes(slope = -1, intercept = 1), size = .5, alpha = .5) +
+  facet_grid(factors_rec * items_per_factor_rec ~ loading_rec * factor_cor_rec) +
+  guides(color = guide_legend(override.aes = list(alpha = 1, size = 1.5))) +
+  scale_x_continuous(breaks = c(0.90, 0.94, 0.98, 1.00)) +
+  labs(y = "RMSEA", x = "CFI") +
+  theme_bw() +
+  theme(legend.position = "bottom",
+        panel.spacing.x = unit(14, units = "points"))
+
+ggsave(filename = here("img/cfi_conditional_rmsea_with_factor_cor.png"),
+       height = 10,
+       width = 8,
+       scale = 1.15,
+       dpi = 320)
 
 # ---------------------------------
 

5ea20414f6360a8a4d7310796b6582d580da7180
Justin Kracht <justin.kracht@pm.me>
2022-09-18 13:52:59 -0500

    Final version.

--- misc/compare_factor_loadings.R
+++ misc/compare_factor_loadings.R
@@ -2,6 +2,7 @@ library(fungible)
 library(noisemaker)
 library(future)
 library(furrr)
+library(tidyverse)
 
 reps <- 100
 
@@ -12,26 +13,35 @@ mod <- simFA(Model = list(NFac = 1,
              Loadings = list(FacLoadDist = "sequential",
                              FacLoadRange = c(.7, .3)))
 
-for (i in 1:reps) {
-  Rcb <- noisemaker(mod, method = "CB", target_rmsea = 0.05)
+out <- purrr::map_dfr(.x = 1:reps, .f = function(i) {
+  Rcb <- noisemaker(mod, method = "CB", target_rmsea = 0.08)
   Rtkl <- noisemaker(mod, method = "TKL", 
                      target_rmsea = Rcb$rmsea,
                      target_cfi = Rcb$cfi,
                      tkl_ctrl = list(penalty = 0))
   
-  # cb_fac <- factanal(covmat = Rcb$Sigma, factors = 1, 
-  #                    n.obs = 1000, rotation = "none")
-  # tkl_fac <- factanal(covmat = Rtkl$Sigma, factors = 1, 
-  #                     n.obs = 1000, rotation = "none")
-  
   cb_fac_rot <- faMain(R = Rcb$Sigma, n = 1000, numFactors = 1L, 
                        facMethod = "faml", rotate = "targetT",
                        targetMatrix = mod$loadings)$loadings |>  unclass()
   tkl_fac_rot <- faMain(R = Rtkl$Sigma, n = 1000, numFactors = 1L, 
-                       facMethod = "faml", rotate = "targetT",
-                       targetMatrix = mod$loadings)$loadings |>  unclass()
+                        facMethod = "faml", rotate = "targetT",
+                        targetMatrix = mod$loadings)$loadings |>  unclass()
   
-  
-}
+  tibble(
+    rep = rep(i, times = 10),
+    item = 1:10,
+    tkl_rmsea = rep(Rtkl$rmsea, 10),
+    tkl_cfi = rep(Rtkl$cfi, 10),
+    cb_rmsea = rep(Rcb$rmsea, 10),
+    cb_cfi = rep(Rcb$cfi, 10),
+    tkl_loadings = tkl_fac_rot[,1],
+    cb_loadings = cb_fac_rot[,1]
+  )
+})
 
-plan(multisession)
+out <- dplyr::mutate(out, loading_diff = tkl_loadings - cb_loadings)
+
+ggplot(out, aes(x = item, y = loading_diff)) +
+  geom_jitter() +
+  geom_hline(aes(yintercept = 0), linetype = "dashed") +
+  theme_bw()
